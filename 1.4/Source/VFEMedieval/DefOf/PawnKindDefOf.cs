2ebcb5cc41fdce51eb7888e74a0e96adca619607
Taranchuk <nfhfyxer@gmail.com>
2022-09-22 02:23:07 +0300

    removed black knight code

--- 1.4/Source/VFEMedieval/DefOf/PawnKindDefOf.cs
+++ 1.4/Source/VFEMedieval/DefOf/PawnKindDefOf.cs
@@ -6,7 +6,6 @@ namespace VFEMedieval
     [DefOf]
     public static class PawnKindDefOf
     {
-        public static PawnKindDef VFEM_BlackKnight;
         public static PawnKindDef VFEM_SellSword;
     }
 }
